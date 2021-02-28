7c0add079033d6450ff7ff8a411a01e392e9f2cf
Amanda <awiehrs.photo@gmail.com>
2021-02-28 13:52:03 -0500

    app utilizes css to display properly now, handlebars and server.js updated

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,3 @@
 INSERT INTO burgers (name, devoured) VALUES ('Farmhouse Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Southwest Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Cheeseburger', false);
