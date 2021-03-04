819b97fe3dc857e9d75d6268ebc58d344cf1e7be
Amanda <awiehrs.photo@gmail.com>
2021-03-03 22:08:34 -0500

    added JawsDB info

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 // Set up MySQL connection.
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'root',
-  database: 'burger_db',
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+      host: "hwr4wkxs079mtb19.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+      port: 3306,
+      user: "x74g7nur4p1chkja",
+      password: "i08rc7qfckdjfip6",
+      database: "qaxegrdktusf6bxd"
+  });
+}
 
 // Make connection.
 connection.connect((err) => {
